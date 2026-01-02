23ba3dd399c326c7fa5999b2a3e8423dc56c3a0d
aditya1010-max <rauta2813@gmail.com>
2026-01-03 00:04:38 +0530

    added text updates

--- frontend/src/components/CollectionSection/CollectionSection.tsx
+++ frontend/src/components/CollectionSection/CollectionSection.tsx
@@ -2,7 +2,7 @@ export default function CollectionSection() {
   return (
     <div>
       <h2>Collection Section</h2>
-      <p>this section is not complete yet</p>
+      <p>this section is not complete yet, work in progress</p>
     </div>
   );
 }
