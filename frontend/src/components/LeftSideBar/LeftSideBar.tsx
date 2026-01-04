bc28372592453e58fd8b8fb281716f89387cd47a
aditya1010-max <rauta2813@gmail.com>
2026-01-04 23:58:58 +0530

    new options added

--- frontend/src/components/LeftSideBar/LeftSideBar.tsx
+++ frontend/src/components/LeftSideBar/LeftSideBar.tsx
@@ -32,6 +32,8 @@ const collections = [
   "Forest",
   "Sunrise",
   "Storm",
+  "Ocean",
+  "Desert",
 ];
 
 
---
+++ frontend/src/components/PaletteCard/PaletteCard.tsx
@@ -0,0 +1,3 @@
+export default function PaletteCard() {
+    return <div>PaletteCard</div>;
+}
