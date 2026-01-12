b0051ecbb6fd32a4a46a1763f400f699b2be28b3
aditya1010-max <rauta2813@gmail.com>
2026-01-12 23:58:57 +0530

    added link to git repo

--- frontend/src/components/CollectionSection/CollectionSection.tsx
+++ frontend/src/components/CollectionSection/CollectionSection.tsx
@@ -4,6 +4,8 @@ export default function CollectionSection() {
       <h2>Collection Section</h2>
       <p>this section is not complete yet, work in progress</p>
       <p>suggest the feature you want me to add</p>
+
+      <p>github link- <a href="https://github.com/aditya1010-max/huego">Your GitHub Repository</a></p>
     </div>
   );
 }
