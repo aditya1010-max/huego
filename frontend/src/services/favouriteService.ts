fe8c46d25c5b84d43dd4f7d2abcd0a56e393ff28
aditya1010-max <rauta2813@gmail.com>
2026-04-01 12:14:45 +0530

    update user.ts to renderr backend API

--- frontend/src/services/favouriteService.ts
+++ frontend/src/services/favouriteService.ts
@@ -1,8 +1,10 @@
 import { getUserId } from "../utils/user";
+const API_BASE = import.meta.env.VITE_API_URL;
+
 
 export async function getFavoritePalettes() {
   const userId = getUserId();
-  const res = await fetch(`http://localhost:5000/api/palettes/favorites/${userId}`);
+  const res = await fetch(`${API_BASE}/api/palettes/${userId}`);
 
   return await res.json();
 }
